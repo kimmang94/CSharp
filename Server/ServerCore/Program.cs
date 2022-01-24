9de2e667efca9e790ad003fb9d69fc6b9a738e95
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2022-01-25 01:49:25 +0900

    Add files via upload
    Server Programming _ Thread

---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread()
+        {
+            Console.WriteLine("Hello Thread");
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(MainThread);
+            t.Start();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
