dc246ca4191364404e07b5f01ce4e82890c7b613
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2022-02-24 09:29:17 +0900

    write&writeLine 차이점

---
+++ write&writeLine.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write"); Console.Write("Write");
+            Console.Write("Write");
+            Console.Write("Write");
+            Console.Write("Write");
+
+            Console.WriteLine("WriteLine"); Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+        }
+    }
+}
